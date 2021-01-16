0a33c27e66f6eab88b3d598f156af9cc27565075
theolaa <38194309+theolaa@users.noreply.github.com>
2021-01-15 16:03:36 -0800

    Update app.js

--- app.js
+++ app.js
@@ -10,5 +10,6 @@ app.use('/', function(req,res){
     //__dirname : It will resolve to your project folder.
   });
 const server = http.createServer(app);
-server.listen(process.env.PORT || 8080);
+port = (process.env.PORT || 8080)
+server.listen(port);
 console.debug('Server listening on port ' + port);
