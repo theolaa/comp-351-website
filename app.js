439bc2d295b74225ba018aa4c14f5ead794173fe
theolaa <38194309+theolaa@users.noreply.github.com>
2021-02-04 01:48:03 -0800

    Filesystem finished (I think...)

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
+// Modules
 const http = require('http');
 const url = require('url');
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+
+// Set up Server
 const app = express();
-app.use(express.json());
-app.use(express.static("express"));
-// default URL for website
-app.use('/', function(req,res){
-    res.sendFile(path.join(__dirname+'/express/index.html'));
-    //__dirname : It will resolve to your project folder.
+
+app.get("/COMP351/Labs/lab4/writeFile", function (req, res) {
+  let q = url.parse(req.url, true);
+  if (q.query["text"]) {
+    fs.writeFile(path.join(__dirname + "/express/COMP351/Labs/lab4/readFile/file.txt"), q.query["text"], function () {
+      res.send("'" + q.query["text"] + "' saved to lab4/readFile/file.txt");
+    });
+  } else {
+    res.send("Please use 'text' as the query <br> Example: '?text=Hello'");
+  }
+});
+app.get("/COMP351/Labs/lab4/readFile/*", function (req, res) {
+  let q = url.parse(req.url, true);
+  fs.readFile(path.join(__dirname + "/express/COMP351/Labs/lab4/readFile/" + path.basename(req.url)), function (err, data) {
+    if (err) {
+      res.send("404 " + path.basename(req.url) + " not found");
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end(data);
+    }
   });
+  //res.send(fs.readFileSync(path.normalize(req.url + "../file.txt")));
+});
+app.use(express.static('express'));
+
 const server = http.createServer(app);
 port = (process.env.PORT || 8080)
 server.listen(port);
+
 console.debug('Server listening on port ' + port);
-console.log(__dirname);
