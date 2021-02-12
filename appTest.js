a14b0a43655e6662555c98299d8d43c7b7bb6081
theolaa <38194309+theolaa@users.noreply.github.com>
2021-02-11 20:27:15 -0800

    DB writing successfull

--- appTest.js
+++ appTest.js
@@ -1,18 +1,27 @@
 // Thanks to Adrian De Niz for this static HTML server outline
 
+// Modules
 const http = require('http');
+const mysql = require('mysql');
 const fs = require('fs');
 const path = require('path');
 const url = require('url');
 
-const hostname = process.env.HOST || '127.0.0.1'; // use hostname 127.0.0.1 unless there exists a preconfigured port
-const port = process.env.PORT || 8080; // use port 8080 unless there exists a preconfigured port
+// Global Variables
+var DBconnected = false;
+var DBHost = "us-cdbr-east-03.cleardb.com";
+var DBuser = "b25715a5e769ae";
+var DBpwd = "61476d2a"
+var DBdb = "heroku_3b482e602de5856"
+var hostname = process.env.HOST || '127.0.0.1'; // use hostname 127.0.0.1 unless there exists a preconfigured port
+var port = process.env.PORT || 8080; // use port 8080 unless there exists a preconfigured port
 
 http.createServer(function (req, res) {
   let q = url.parse(path.normalize(req.url), true);
-  console.log('Request: ', q);
+  // console.log('Request: ', q);
   let filePath = "";
 
+  // Lab 4 Write to File
   if (q.pathname == "/COMP351/Labs/lab4/writeFile/") {
     console.log("Writefile");
     if (q.query["text"]) {
@@ -27,15 +36,59 @@ http.createServer(function (req, res) {
       res.end("Please use 'text' as the query <br> Example: '?text=Hello'");
     }
     return;
-  } else if (q.pathname.indexOf("/COMP351/Labs/lab4/readFile/") == 0) {
+  }
+
+  // Lab 4 Read from File
+  else if (q.pathname.indexOf("/COMP351/Labs/lab4/readFile/") == 0) {
     console.log("Readfile " + path.basename(q.pathname));
     filePath = "express/" + q.pathname;
-  } else if (q.pathname == "/COMP351/Labs/lab4/delFile/") {
+  }
+
+  // Lab 4 Delete File
+  else if (q.pathname == "/COMP351/Labs/lab4/delFile/") {
     fs.unlink(path.join(__dirname + "/express/COMP351/Labs/lab4/readFile/file.txt"), function () {
       res.end("file.txt deleted");
       return;
     })
-  } else if (q.pathname.indexOf(".") < 0) {
+  }
+
+  // Lab 5 Write to DB
+  else if (q.pathname.includes("/COMP351/Labs/lab5/write")) {
+    if (DBconnected) {
+      con.query('INSERT INTO score VALUES ("' + q.query.name + '",' + q.query.score + ');', function (err, rows) {
+        temp = res;
+        if (err) {
+          console.log(err);
+          res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
+          res.end("Error writing to DB");
+        } else {
+          console.log(res);
+          res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
+          res.end(q.query.name + ":" + q.query.score + " was stored in the DB");
+        }
+      });
+      return;
+    }
+  }
+
+  // Lab 5 Read from DB
+  else if (q.pathname.includes("/COMP351/Labs/lab5/read")) {
+    if (DBconnected) {
+      con.query('SELECT FROM score WHERE TRUE;', function (err, rows) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+      });
+    }
+  }
+
+  else if (q.pathname.includes("/COMP351/Labs/lab5/read")) {
+
+  }
+
+  // Paths with no specific file e.g. https://localhost/ as opposed to https://localhost/index.html
+  else if (q.pathname.indexOf(".") < 0) {
     console.log("BASEDIR");
     filePath = path.join("express" + q.pathname + "/index.html");
   }
@@ -67,7 +120,7 @@ http.createServer(function (req, res) {
 
   let contentType = mimeTypes[extname] || 'application/octet-stream';
 
-  console.log("File path: " + filePath);
+  // console.log("File path: " + filePath);
   fs.readFile(filePath, function (error, content) {
     if (error) {
       if (error.code == 'ENOENT') {
@@ -89,3 +142,32 @@ http.createServer(function (req, res) {
 
 }).listen(port);
 console.log(`Server running at http://${hostname}:${port}/`);
+
+const con = mysql.createConnection({
+  host: DBHost,
+  database: DBdb,
+  user: DBuser,
+  password: DBpwd
+});
+
+con.connect(function (err) {
+  if (err) {
+    console.log(err);
+    return;
+  } else {
+    console.log("Connected to MySQL!");
+    DBconnected = true;
+  }
+});
+
+// Keep connection alive, otherwise closes after ~30s of inactivity
+setInterval(function () {
+  con.query('SELECT 1 FROM score', function (err, rows) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log('Connection to ' + DBHost + ":" + DBdb + ' Active...');
+  });
+  //}, 5000); // For testing
+}, 45000); // Turns out 45s is slow enough to not feel like I'm wasting DB resources, but frequent enough to still work.
