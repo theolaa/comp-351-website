e5f944ee5bf97881eff8a76f66114e1ebd2113ae
theolaa <38194309+theolaa@users.noreply.github.com>
2021-02-12 19:47:09 -0800

    Actually finally removed Express

--- app.js
+++ app.js
@@ -1,16 +1,150 @@
+// Thanks to Adrian De Niz for this static HTML server template
+
 // Modules
 const http = require('http');
 const mysql = require('mysql');
-const url = require('url');
-const express = require('express');
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
+const url = require('url');
 
 // Global Variables
+var DBconnected = false;
 var DBHost = "us-cdbr-east-03.cleardb.com";
 var DBuser = "b25715a5e769ae";
 var DBpwd = "61476d2a"
 var DBdb = "heroku_3b482e602de5856"
+var hostname = process.env.HOST || '127.0.0.1'; // use hostname 127.0.0.1 unless there exists a preconfigured port
+var port = process.env.PORT || 8080; // use port 8080 unless there exists a preconfigured port
+
+http.createServer(function (req, res) {
+  let q = url.parse(path.normalize(req.url), true);
+  // console.log('Request: ', q);
+  let filePath = "";
+
+  // Lab 4 Write to File
+  if (q.pathname == "/COMP351/Labs/lab4/writeFile/") {
+    console.log("Writefile");
+    if (q.query["text"]) {
+      fs.appendFile(path.join(__dirname + "/public/COMP351/Labs/lab4/readFile/file.txt"), q.query["text"] + "\n", function (err) {
+        if (err) {
+          res.end("Error");
+        } else {
+          res.end("'" + q.query["text"] + "' saved to lab4/readFile/file.txt");
+        }
+      });
+    } else {
+      res.end("Please use 'text' as the query <br> Example: '?text=Hello'");
+    }
+    return;
+  }
+
+  // Lab 4 Read from File
+  else if (q.pathname.indexOf("/COMP351/Labs/lab4/readFile/") == 0) {
+    console.log("Readfile " + path.basename(q.pathname));
+    filePath = "public/" + q.pathname;
+  }
+
+  // Lab 4 Delete File
+  else if (q.pathname == "/COMP351/Labs/lab4/delFile/") {
+    fs.unlink(path.join(__dirname + "/public/COMP351/Labs/lab4/readFile/file.txt"), function () {
+      res.end("file.txt deleted");
+      return;
+    })
+  }
+
+  // Lab 5 Write to DB
+  else if (q.pathname.includes("/COMP351/Labs/lab5/write")) {
+    if (DBconnected) {
+      con.query('INSERT INTO score VALUES ("' + q.query.name + '",' + q.query.score + ');', function (err, rows) {
+        temp = res;
+        if (err) {
+          console.log(err);
+          res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
+          res.end("Error writing to DB");
+        } else {
+          res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
+          res.end(q.query.name + ":" + q.query.score + " was stored in the DB");
+        }
+      });
+      return;
+    }
+  }
+
+  // Lab 5 Read from DB
+  else if (q.pathname.includes("/COMP351/Labs/lab5/read")) {
+    if (DBconnected) {
+      con.query('SELECT * FROM score;', function (err, result, fields) {
+        if (err) {
+          console.log(err);
+          res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
+          res.end("Error reading from DB");
+        } else {
+          console.log(result);
+          res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
+          for (i = 0; i < result.length; i++) {
+            res.write(result[i].name + ":" + result[i].score + "\n");
+          }
+          res.end();
+        }
+      });
+      return;
+    }
+  }
+
+  // Paths with no specific file e.g. https://localhost/ as opposed to https://localhost/index.html
+  else if (q.pathname.indexOf(".") < 0) {
+    filePath = path.join("public" + q.pathname + "/index.html");
+  }
+  else {
+    filePath = 'public' + req.url;
+  }
+
+  let extname = String(path.extname(filePath)).toLowerCase();
+  let mimeTypes = {
+    '.html': 'text/html',
+    '.htm': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+    '.svg': 'image/svg+xml',
+    '.wav': 'audio/wav',
+    '.mp4': 'video/mp4',
+    '.woff': 'application/font-woff',
+    '.ttf': 'application/font-ttf',
+    '.eot': 'application/vnd.ms-fontobject',
+    '.otf': 'application/font-otf',
+    '.wasm': 'application/wasm',
+    '.ico': 'image/x-icon',
+    '.txt': 'text/plain'
+  };
+
+  let contentType = mimeTypes[extname] || 'application/octet-stream';
+
+  // console.log("File path: " + filePath);
+  fs.readFile(filePath, function (error, content) {
+    if (error) {
+      if (error.code == 'ENOENT') {
+        fs.readFile('public/404.html', function (error, content) {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          res.end(content, 'utf-8');
+        });
+      }
+      else {
+        res.writeHead(500);
+        res.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
+      }
+    }
+    else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+
+}).listen(port);
+console.log(`Server running at http://${hostname}:${port}/`);
 
 const con = mysql.createConnection({
   host: DBHost,
@@ -25,64 +159,18 @@ con.connect(function (err) {
     return;
   } else {
     console.log("Connected to MySQL!");
+    DBconnected = true;
   }
 });
 
 // Keep connection alive, otherwise closes after ~30s of inactivity
 setInterval(function () {
-  con.query('SELECT name,score FROM score', (err, rows) => {
+  con.query('SELECT 1 FROM score', function (err, rows) {
     if (err) {
       console.log(err);
       return;
     }
-    console.log('Connection Active...');
+    console.log('DB Connection active (' + DBHost + ":" + DBdb + ')');
   });
-}, 45000);
-
-con.query('SELECT * FROM score', (err, rows) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log('Contents of ' + DBdb + ':');
-  console.log(rows);
-});
-
-// Set up Server
-const app = express();
-
-app.get("/COMP351/Labs/lab4/writeFile", function (req, res) {
-  let q = url.parse(req.url, true);
-  if (q.query["text"]) {
-    fs.app
-    fs.appendFile(path.join(__dirname + "/express/COMP351/Labs/lab4/readFile/file.txt"), q.query["text"] + "\n", function () {
-      res.send("'" + q.query["text"] + "' saved to lab4/readFile/file.txt");
-    });
-  } else {
-    res.send("Please use 'text' as the query <br> Example: '?text=Hello'");
-  }
-});
-app.get("/COMP351/Labs/lab4/readFile/*", function (req, res) {
-  let q = url.parse(req.url, true);
-  fs.readFile(path.join(__dirname + "/express/COMP351/Labs/lab4/readFile/" + path.basename(req.url)), function (err, data) {
-    if (err) {
-      res.send("404 " + path.basename(req.url) + " not found");
-    } else {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end(data);
-    }
-  });
-  //res.send(fs.readFileSync(path.normalize(req.url + "../file.txt")));
-});
-app.get("/COMP351/Labs/lab4/delFile", function (req, res) {
-  fs.unlink(path.join(__dirname + "/express/COMP351/Labs/lab4/readFile/file.txt"), function () {
-    res.send("file.txt deleted");
-  })
-});
-app.use(express.static('express'));
-
-const server = http.createServer(app);
-port = (process.env.PORT || 8080)
-server.listen(port);
-
-console.debug('Server listening on port ' + port);
+  //}, 5000); // For testing
+}, 45000); // Turns out 45s is slow enough to not feel like I'm wasting DB resources, but frequent enough to still work.
